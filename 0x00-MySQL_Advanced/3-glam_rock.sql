6c7578aa1ddcb0c4d3dc522d2961f4c82073cd74
SafaFarah <kangjisook13@gmail.com>
2024-08-09 01:52:22 +0300

    create 6-bonus.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,9 +2,13 @@
 -- The columns in the output are:
 -- - band_name: The name of the band
 -- - lifespan: The lifespan of the band in years until 2022
+
 SELECT 
     name AS band_name,
-    IFNULL(split, 2022) - formed AS lifespan
+    CASE
+        WHEN split IS NULL THEN 2022 - formed
+        ELSE split - formed
+    END AS lifespan
 FROM metal_bands
 WHERE style = 'Glam rock'
 ORDER BY lifespan DESC;
---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,10 @@
+-- Trigger to reset valid_email if email is changed
+DELIMITER //
+CREATE TRIGGER reset_valid_email BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email != OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END //
+DELIMITER ;
