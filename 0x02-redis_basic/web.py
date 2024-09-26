26b2d3f6e6869557ee27cd3c5591bb6ec51ed43d
SafaFarah <kangjisook13@gmail.com>
2024-09-26 22:40:25 +0300

    update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,40 @@
 #!/usr/bin/env python3
 """
-Caching request module
+A module to fetch a webpage and cache its content using Redis.
 """
+
 import redis
 import requests
 from functools import wraps
 from typing import Callable
 
 
-def track_get_page(fn: Callable) -> Callable:
-    """ Decorator for get_page
+def count_requests(method: Callable) -> Callable:
     """
-    @wraps(fn)
+    A decorator to count how many times a particular URL is accessed.
+    """
+    @wraps(method)
     def wrapper(url: str) -> str:
-        """ Wrapper that:
-            - check whether a url's data is cached
-            - tracks how many times get_page is called
+        """The wrapper function for caching the output.
         """
-        client = redis.Redis()
-        client.incr(f'count:{url}')
-        cached_page = client.get(f'{url}')
+        r = redis.Redis()
+        r.incr(f'count:{url}')
+        cached_page = r.get(f'{url}')
         if cached_page:
             return cached_page.decode('utf-8')
-        response = fn(url)
-        client.set(f'{url}', response, 10)
+        response = method(url)
+        r.set(f'{url}', response, 10)
         return response
     return wrapper
 
 
-@track_get_page
+@count_requests
 def get_page(url: str) -> str:
-    """ Makes a http request to a given endpoint
     """
-    response = requests.get(url)
-    return response.text
+    Get content of a URL, caches it, and sets an expiration time of 10 seconds.
+    Args:
+        url (str): The URL to fetch the HTML content from.
+    Returns:
+        str: The HTML content of the page.
+    """
+    return requests.get(url).text
